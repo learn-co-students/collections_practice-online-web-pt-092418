3dead881c32ecf7f41ccd072ae6a8a23a0a981dd
Anuj Panchal <anujpan1@gmail.com>
2018-10-04 19:06:39 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by {|string| string.length}
+end
+
+def swap_elements(array)
+  replace = array[1]
+  array.delete_at(1)
+  array.push(replace)
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |c|
+    c[2] = "$"
+  end
+end
+
+def find_a(array)
+  array.select do |c|
+    c.start_with?("a")
+  end
+end
+
+def sum_array(array)
+  array.inject do | a , b | 
+    a + b
+  end
+end
+
+def add_s(array)
+  array.each_with_index do |a|
+    a += "s"
+    array.push(a)
+  end
+end   
