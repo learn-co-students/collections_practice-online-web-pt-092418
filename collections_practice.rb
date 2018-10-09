79d25a847416c00ae42eb67e60835fe0b4faca99
Emilio Betancourt <embeor@gmail.com>
2018-10-09 16:14:46 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,20 @@
+def sort_array_asc (integers)
+  integers.sort
+end
 
+def sort_array_desc (integers)
+  integers.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count (integers)
+  integers.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements (array)
+
+
+end
