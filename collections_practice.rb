62a5a027fcf2555276864f473ec6c97faa7296aa
Emilio Betancourt <embeor@gmail.com>
2018-10-10 01:57:35 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -15,6 +15,36 @@ def sort_array_char_count (integers)
 end
 
 def swap_elements (array)
+  array [0], array [1], array [2] = array [0], array [2], array [1]
+end
 
+def reverse_array (array)
+  array [0], array [1], array [2] = array [2], array [1], array [0]
+end
 
+def kesha_maker (array)
+  array.each do |a|
+    a.insert 2, "$"
+    a[3,1] = ""
+  end
+end
+
+def find_a (array)
+  array.select do |word|
+    word.start_with? "a"
+  end
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s (array)
+  array.each_with_index.collect do |word,index|
+    if index !=1
+        word = word + "s"
+    else
+      word = word
+    end
+  end
 end
